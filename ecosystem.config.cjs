baf00fb14e76429b0a8a33c6800bb90c2aa8138a
en-tomoda <kazuya_tomoda@en-japan.com>
2026-06-10 23:39:15 +0900

    fix: npm run startをやめてnext経由で起動させる方式に変更

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,17 +2,20 @@ module.exports = {
   apps: [
     {
       name: "css-proto",
-      script: "npm",
-      args: "run start",
+      script: "./node_modules/next/dist/bin/next",
+      args: "start -p 3001 -H 0.0.0.0",
+      interpreter: "node",
       cwd: __dirname,
       instances: 1,
       exec_mode: "fork",
       autorestart: true,
       watch: false,
       max_memory_restart: "512M",
+      time: true,
       env: {
         NODE_ENV: "production",
         PORT: 3001,
+        HOSTNAME: "0.0.0.0",
       },
     },
   ],
